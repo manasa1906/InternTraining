2d79fcc0ba47dc4b7d4084e264953a71b197eb75
manasa1906 <158132778+manasa1906@users.noreply.github.com>
2024-02-02 14:38:21 +0530

    SpringBasics

---
+++ game/superContraGame.java
@@ -0,0 +1,21 @@
+package com.Intern1906.Spring_Freamework.game;
+
+public class superContraGame implements gamingConsole
+{
+	public void up()
+	  {
+		  System.out.println("Up");
+	  }
+	  public void down()
+	  {
+		  System.out.println("Sit Down");
+	  }
+	  public void left()
+	  {
+		  System.out.println("Go back");
+	  }
+	  public void right()
+	  {
+		  System.out.println("Shoot a Bullet");
+	  }
+}
